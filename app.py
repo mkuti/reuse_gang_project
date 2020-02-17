164eade94b5821e61ccc1e861f02707e25b06727
Maripia Kuti <pionste@gmail.com>
2020-02-17 23:19:31 +0000

    moved searchbar and add button to home page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import os
-from flask import Flask, render_template, redirect, request, url_for
+import os, json
+from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from os import path
-if path.exists("env.py"):  # import file where username and password of MongoDB is saved
+# import file where username and password of MongoDB is saved
+if path.exists("env.py"):
     import env
 
 app = Flask(__name__)  # create instance of flask
@@ -15,23 +17,28 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 app.config["MONGODB_NAME"] = "reuse-gang"
 app.config["MONGO_URI"] = MONGO_URI
 
-mongo = PyMongo(app)  # create an instance of Pymongo with app object being pushed as argument
+# create an instance of Pymongo with app object being pushed as argument
+mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('/pages/home.html', items=mongo.db.items.find(), users=mongo.db.users.find())
+    return render_template('/pages/home.html', items=mongo.db.items.find(),
+                           users=mongo.db.users.find())
+
 
 @app.route('/login')
 def login():
     return render_template('/components/login.html')
 
-@app.route('/register')
+
+@app.route('/register', methods=["POST"])
 def register():
     return render_template('/components/register.html')
 
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP", "0.0.0.0"),
-            port=int(os.getenv("PORT", "5000")), 
+            port=int(os.getenv("PORT", "5000")),
             debug=True)
