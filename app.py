a00f0c6b28e3b0b4322bab45f00acbc355ebe4ba
Maripia Kuti <pionste@gmail.com>
2020-02-29 17:36:49 +0000

    added flash message to add item function

--- app.py
+++ app.py
@@ -71,6 +71,7 @@ def add_item():
     if request.method == "POST":
         items = mongo.db.items
         items.insert_one(request.form.to_dict())
+        flash("Your free stuff will be shared immediately with the gang")
         return redirect(url_for('home'))
     return render_template('/pages/additem.html', categories=categories)
 
